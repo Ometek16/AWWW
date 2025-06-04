dcd467768fefc5639635d4b32b5bba0ae18bfcc2
Ometek <kuba.ornatek@gmail.com>
2025-06-04 21:20:43 +0200

    huh 2

--- roulette/roulette_project/settings.py
+++ roulette/roulette_project/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8*t5ho6hf^!_01ha!(@uoe58f#zk)uks4ecqju%_0cxcp*k_we'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
